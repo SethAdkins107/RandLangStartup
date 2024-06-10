b0b4e1c9dcdbb0324cfd32deb7f7ea28a6f0bb5e
SethA <adkinsseth10@gmail.com>
2024-06-10 18:05:36 -0400

    First Commit, probably not many more

---
+++ randLangStartup.py
@@ -0,0 +1,19 @@
+import subprocess
+import random
+
+def changeLang(langList) :
+    subprocess.run(["powershell.exe", f"Set-WinUserLanguageList {langList}"])
+
+def main() :
+    randNum = random.randint(1, 2)
+    randNum = 2
+
+    if (randNum == 1):
+        lList = '("es-MX", "en-US", "fr-FR")'
+    elif (randNum == 2):
+        lList = '("fr-FR", "en-US", "es-MX")'
+
+    changeLang(lList)
+
+if __name__ == "__main__":
+    main()
