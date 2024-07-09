9f64c8caee2890e1a55aa4a52d765f4257051ff9
SethA <adkinsseth10@gmail.com>
2024-07-09 16:22:34 -0400

    Fixed accidental deletion

--- Startup.bat
+++ Startup.bat
@@ -1,8 +1,2 @@
-rem Keep powershell from opening a window on execution
 @echo off
-
-rem Runs my python language change automation script w/o opening powershell
-start "" /B "C:\Users\adkin\anaconda3\pythonw.exe" "C:\Users\adkin\OneDrive\Documents\Work\PersonalProjects\StartupAuto\randLangStartup.py"
-
-rem Opens ChatGPT at startup to always have my Jarvis
-start "" "https://chatgpt.com/"
+start "" "C:\Users\adkin\anaconda3\pythonw.exe" "C:\Users\adkin\OneDrive\Documents\Work\PersonalProjects\StartupAuto\randLangStartup.py"
